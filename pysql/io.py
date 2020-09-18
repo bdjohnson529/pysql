82dc56ac7b94e686ded1079d7c5d59f2a5e37e74
Ben Johnson <bdjohnson529@gmail.com>
2020-09-18 12:40:45 -0700

    add BCP write function

--- pysql/io.py
+++ pysql/io.py
@@ -27,31 +27,34 @@ def getTableSchema(server, database, table):
 
 
 
-def executeQuery(server, database, query, print_feedback=False):
+def readTableFromQuery(server_name, db_name, query):
     """
     Read table from SQL query, using Pyodbc.
 
-    :param server: Server name
-    :type server: str
-    :param database: Database name
-    :type database: str
+    :param server_name: Server name
+    :type server_name: str
+    :param db_name: Database name
+    :type db_name: str
     :param query: SQL query
     :type query: str
     """
-    if(print_feedback):
-        print(query)
 
     # Create the connection
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';Trusted_Connection=yes')
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server_name + 
+                          ';DATABASE=' + db_name + ';Trusted_Connection=yes')
 
     # Read in query
     start = time.time()
     input_df = pd.io.sql.read_sql(query, conn)
     end = time.time()
 
-
     # Print descriptive statistics
-    print("Read time :\t", int(end-start), "s")
+    print("\nRead time :\t", int(end-start), "s")
+    rows = input_df.shape[0]
+    cols = input_df.shape[1]
+
+    print(f"Rows : \t\t {rows:,d}")
+    print(f"Columns : \t {cols:,d}")
 
     # Close connection
     conn.close()
@@ -98,7 +101,32 @@ def executeQueryFromFile(server, database, file_name):
     return df
 
 
-def writeDfToSQL(df, server, database, table, driver='SQL+Server', chunksize=200):
+def writeDfToSQL(df, server, database, table, method='Pandas', driver='SQL+Server', chunksize=200):
+    """
+    Writes pandas dataframe to SQL, using either BCP or Pandas.
+
+    :param df: table
+    :type df: pd.DataFrame
+    :param server: SQL Server - Server name
+    :type server: str
+    :param database: SQL Server - Database name
+    :type database: str
+    :param table: SQL Server - Table name
+    :type table: str
+    :param method: Write method (either 'Pandas', or 'BCP')
+    :type method: str
+    """
+    if(method == 'Pandas'):
+        writeWithPandas(df, server, database, table, driver, chunksize)
+    elif(method == 'BCP'):
+        writeWithBCP(df, server, database, table)
+    else:
+        print("Invalid method specified.")
+
+    return None
+
+
+def writeWithPandas(df, server, database, table, driver='SQL+Server', chunksize=200):
     """
     Wrapper for pd.to_sql()
 
@@ -134,3 +162,42 @@ def writeDfToSQL(df, server, database, table, driver='SQL+Server', chunksize=200
 
     # diagnostic timing
     print("Write time :\t", int(end - start), " s")
+
+    return None
+
+def writeWithBCP(server, database, table, df):
+    """
+    Writes a pandas dataframe to a SQL server instance, using the BCP utility.
+    :param server: Server name
+    :type server: str
+    :param database: Database name
+    :type database: str
+    :param df: dataframe
+    :type df: pd.DataFrame
+    """
+    if server not in ('KSITEST', 'KSISTAGING'):
+        print("Write permitted only on KISTEST, KSISTAGING")
+        return False
+
+    # write dataframe to temp file
+    os.makedirs('tmp', exist_ok=True)
+    tmp_file = os.path.abspath("tmp/" + randomWord(10) + ".txt")
+
+
+    # truncate table on destination server
+    query = f"TRUNCATE TABLE {database}.dbo.{table}"
+    cmd = f"sqlcmd -S {server} -h -1 -Q \"{query}\""
+    os.system(cmd)
+
+    # write df to temp file
+    df.to_csv(tmp_file, index=False, sep="|")
+
+    # upload temp file to destination server
+    cmd = f"bcp {database}.dbo.{table} in \"{tmp_file}\" -c -T -t\"|\" -S {server}"
+    os.system(cmd)
+
+
+    # delete temp file
+    shutil.rmtree('tmp')
+
+    return None
